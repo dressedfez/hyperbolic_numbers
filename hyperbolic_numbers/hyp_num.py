70cabcb39f40b5a48f36cb0bfbb058562053ab9d
Frank Zimmer <frank.e.zimmer@gmail.com>
2021-03-04 19:56:56 +0100

    Adds modulus method

--- hyperbolic_numbers/hyp_num.py
+++ hyperbolic_numbers/hyp_num.py
@@ -3,20 +3,26 @@ class Hyp_num:
         self.x = x
         self.y = y
 
-def add(self, summand):
-    """
-    Add the summand to the hyperbolic number.
-    """
-    result = Hyp_num(0,0)
-    result.x = self.x + summand.x
-    result.y = self.y + summand.y
-    return result
+    def add(self, summand):
+        """
+        Add the summand to the hyperbolic number.
+        """
+        result = Hyp_num(0,0)
+        result.x = self.x + summand.x
+        result.y = self.y + summand.y
+        return result
 
-def multiply(self, factor):
-    """
-    Multiply the factor with the hyperbolic number.
-    """
-    result = Hyp_num(0,0)
-    result.x = self.x*factor.y + self.y*factor.y
-    result.y = self.x*factor.y + self.y*factor.x
-    return result
+    def multiply(self, factor):
+        """
+        Multiply the factor with the hyperbolic number.
+        """
+        result = Hyp_num(0,0)
+        result.x = self.x*factor.y + self.y*factor.y
+        result.y = self.x*factor.y + self.y*factor.x
+        return result
+
+    def modulus(self):
+        """
+        Determines the modulus of the hyperbolic number.
+        """
+        return self.x*self.x-self.y*self.y
